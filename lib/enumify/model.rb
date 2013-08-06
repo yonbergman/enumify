df6927b6d43a9e9aee078834be1d6ae63cc884f3
yon <Lighthawky@gmail.com>
2013-08-06 10:12:02 +0300

    fixed deprecation warning when using scopes

--- lib/enumify/model.rb
+++ lib/enumify/model.rb
@@ -42,7 +42,7 @@ module Enumify
             send("_set_#{parameter.to_s}", opt, true)
         end
 
-        scope opt.to_sym, where(parameter.to_sym => opt.to_s)
+        scope opt.to_sym, lambda { where(parameter.to_sym => opt.to_s) }
       end
 
       # We want to first define all the "positive" scopes and only then define
@@ -52,7 +52,7 @@ module Enumify
         # be used in a joined query with other models that have the same enum field then
         # it will fail on ambiguous column name.
         unless respond_to?("not_#{opt}")
-          scope "not_#{opt}", where("#{self.table_name}.#{parameter} != ?", opt.to_s)
+          scope "not_#{opt}", lambda { where("#{self.table_name}.#{parameter} != ?", opt.to_s) }
         end
       end
 

21a8078044decfe20349fe7bb9a0cdacbdaf621f
yon <Lighthawky@gmail.com>
2014-09-26 11:38:34 -0700

    added guard and updated rspec syntax to expect

--- spec/enumify/enum_spec.rb
+++ spec/enumify/enum_spec.rb
@@ -50,7 +50,7 @@ describe :Enumify do
     describe "model allowing enum value to be nil" do
       subject { @obj_allowing_nil }
       it "should be valid" do
-        subject.should be_valid
+        expect(subject).to be_valid
       end
 
       it 'should not raise error when setting value to nil' do
@@ -58,21 +58,21 @@ describe :Enumify do
           subject.status = nil
         }.to_not raise_error
 
-        subject.status.should be_nil
+        expect(subject.status).to be_nil
       end
     end
 
     describe "model not allowing enum value to be nil" do
       subject { @obj_not_allowing_nil }
       it "should be invalid" do
-        subject.should be_invalid
+        expect(subject).to be_invalid
       end
 
       it 'should not raise error when setting value to nil' do
         expect {
           subject.status = nil
         }.to_not raise_error
-        subject.should be_invalid
+        expect(subject).to be_invalid
       end
     end
 
@@ -81,11 +81,11 @@ describe :Enumify do
   describe "short hand methods" do
     describe "question mark (?)" do
       it "should return true if value of enum equals a value" do
-        @obj.available?.should be_true
+        expect(@obj.available?).to be_truthy
       end
 
       it "should return false if value of enum is different " do
-        @obj.canceled?.should be_false
+        expect(@obj.canceled?).to be_falsey
       end
 
     end
@@ -93,13 +93,13 @@ describe :Enumify do
     describe "exclemation mark (!)" do
       it "should change the value of the enum to the methods value" do
         @obj.canceled!
-        @obj.status.should == :canceled
+        expect(@obj).to be_canceled
       end
 
       context 'trying to set the value to the same value' do
         before { @obj.available! }
         it 'should not save the object again' do
-          @obj.should_not_receive(:save)
+          expect(@obj).to_not receive(:save)
           @obj.available!
         end
       end
@@ -108,17 +108,19 @@ describe :Enumify do
 
     it "should have two shorthand methods for each possible value" do
       Model::STATUSES.each do |val|
-        @obj.respond_to?("#{val}?").should be_true
-        @obj.respond_to?("#{val}!").should be_true
+        expect(@obj.respond_to?("#{val}?")).to be_truthy
+        expect(@obj.respond_to?("#{val}!")).to be_truthy
       end
     end
   end
 
   describe "getting value" do
     it "should always return the enums value as a symbol" do
-      @obj.status.should == :available
-      @obj.status = "canceled"
-      @obj.status.should == :canceled
+      expect {
+        @obj.status = "canceled"
+      }.to change{@obj.status}
+           .from(:available)
+           .to(:canceled)
     end
 
   end
@@ -126,43 +128,43 @@ describe :Enumify do
   describe "setting value" do
     it "should except values as symbol" do
       @obj.status = :canceled
-      @obj.canceled?.should be_true
+      expect(@obj).to be_canceled
     end
 
     it "should except values as string" do
       @obj.status = "canceled"
-      @obj.canceled?.should be_true
+      expect(@obj).to be_canceled
     end
   end
 
   describe "validations" do
+    let (:obj) { Model.new(:status => :available) }
+
     it "should not except a value outside the given list" do
-      @obj = Model.new(:status => :available)
-      @obj.status = :foobar
-      @obj.should_not be_valid
+      obj.status = :foobar
+      expect(obj).to_not be_valid
     end
 
     it "should except value in the list" do
-      @obj = Model.new(:status => :available)
-      @obj.status = :canceled
-      @obj.should be_valid
+      obj.status = :canceled
+      expect(obj).to be_valid
     end
   end
 
   describe "callbacks" do
     it "should receive a callback on change of value" do
-      @obj.should_receive(:status_changed).with(:available,:canceled)
+      expect(@obj).to receive(:status_changed).with(:available,:canceled)
       @obj.canceled!
     end
 
     it "should not receive a callback on initial value" do
       @obj = Model.new
-      @obj.should_not_receive(:status_changed).with(nil, :canceled)
+      expect(@obj).to_not receive(:status_changed)
       @obj.canceled!
       end
 
     it "should not receive a callback on value change to same" do
-      @obj.should_not_receive(:status_changed).with(:available, :available)
+      expect(@obj).to_not receive(:status_changed)
       @obj.available!
     end
 
@@ -170,29 +172,29 @@ describe :Enumify do
 
   describe "scopes" do
     it "should return objects with given value" do
-      Model.available.should == [@obj]
-      Model.canceled.should == [@canceled_obj]
+      expect(Model.available).to eq [@obj]
+      expect(Model.canceled).to eq [@canceled_obj]
     end
 
     it "should return objects with given value when joined with models who have the same enum field" do
-      OtherModel.joins(:model).active.should == [@active_obj]
+      expect(OtherModel.joins(:model).active).to eq [@active_obj]
     end
 
     describe "negation scopes" do
 
       it "should return objects that do not have the given value" do
-        Model.not_available.should include(@canceled_obj, @completed_obj)
+        expect(Model.not_available).to include(@canceled_obj, @completed_obj)
       end
 
       it "should return objects that do not have the given value when joined with models who have the same enum field" do
-        OtherModel.joins(:model).not_active.should include(@expired_obj, @not_expired_obj)
+        expect(OtherModel.joins(:model).not_active).to include(@expired_obj, @not_expired_obj)
       end
 
       it "should not override positive scopes" do
         # We want here to verify that the not_expired scope return only the models with
         # status == "not_expired" and not all the models with status != "expired",
         # since negation scopes should not override the "positive" scopes.
-        OtherModel.not_expired.should == [@not_expired_obj]
+        expect(OtherModel.not_expired).to eq [@not_expired_obj]
       end
 
     end
@@ -201,7 +203,7 @@ describe :Enumify do
 
 
   it "class should have a CONST that holds all the available options of the enum" do
-    Model::STATUSES.should == [:available, :canceled, :completed]
+    expect(Model::STATUSES).to eq [:available, :canceled, :completed]
   end
 
 end
